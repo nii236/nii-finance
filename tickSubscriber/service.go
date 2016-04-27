f09d500337b5b037e6cdce2a5b420ff8f8071c2d
John Nguyen <jtnguyen236@gmail.com>
2016-04-27 12:21:47 +0800

    protobuf changed from int32 to double, early stage NATS publish

--- tickSubscriber/service.go
+++ tickSubscriber/service.go
@@ -5,13 +5,17 @@ import (
 	"github.com/micro/cli"
 	micro "github.com/micro/go-micro"
 	"github.com/micro/go-micro/cmd"
+	"github.com/micro/go-micro/metadata"
+	"github.com/nii236/nii-finance/tickRecorder/proto"
 	"github.com/nii236/nii-finance/tickSubscriber/client"
+	"golang.org/x/net/context"
 )
 
 var (
 	natsURL  = "192.168.99.100:32773"
 	log      *logrus.Logger
 	settings = settingsContainer{}
+	service  micro.Service
 )
 
 type settingsContainer struct {
@@ -27,7 +31,7 @@ func main() {
 	cmd.Init()
 	log = logrus.New()
 
-	service := micro.NewService(
+	service = micro.NewService(
 		micro.Flags(cli.StringFlag{
 			Name:   "pair",
 			Value:  "USD",
@@ -49,4 +53,20 @@ func main() {
 	service.Init(getOptions)
 	go exec(settings.pair, settings.currency)
 	select {}
+}
+
+func pub(time int64, bid float64, ask float64, last float64) {
+	tick := &tickRecorder.Tick{
+		Time: time,
+		Bid:  bid,
+		Ask:  ask,
+		Last: last,
+	}
+	p := service.Client().NewPublication("go.micro.srv.tickRecorder", tick)
+	ctx := metadata.NewContext(context.Background(), map[string]string{
+		"X-User-Id": "nii236",
+		"X-From-Id": "tickSubscriber",
+	})
+	service.Client().Publish(ctx, p)
+
 }
