f02865258f527d2174912587d98c5e85a2500610
John Nguyen <jtnguyen236@gmail.com>
2016-06-27 10:45:53 +0800

    added webui to services

--- services/web/main.go
+++ services/web/main.go
@@ -16,7 +16,7 @@ func main() {
 	app.Action = func(context *ccli.Context) { ccli.ShowAppHelp(context) }
 	cmd.Init(
 		cmd.Name("micro"),
-		cmd.Description("This version of micro contains only the micro web UI"),
+		cmd.Description("This version of micro contains only the web UI"),
 		cmd.Version("latest"),
 	)
 }
